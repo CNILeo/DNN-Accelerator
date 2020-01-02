ef1f15c383975a82a50e9dcee3964a490c8a7029
CNILeo <leo@stu.tjpu.edu.cn>
2020-01-03 01:36:46 +0800

    Update Quantize.py

--- Train And Quantize CNN/Quantize.py
+++ Train And Quantize CNN/Quantize.py
@@ -10,41 +10,36 @@ import numpy as np
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device("cpu")
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--Folder', default='./Model', help='Folder Path To Output The Model') #模型保存路径
-parser.add_argument('--Net', default='./Model/Net.pth', help="Path To Load The Model")  #模型加载路径
+parser.add_argument('--Folder', default='./Model', help='Folder Path To Output The Model')
+parser.add_argument('--Net', default='./Model/Net.pth', help="Path To Load The Model")
 opt = parser.parse_args()
 
-# 超参数设置
-EPOCH = 8   #遍历数据集次数
-BATCH_SIZE = 64      #批处理尺寸(batch_size)
-LR = 0.001        #学习率
+EPOCH = 8
+BATCH_SIZE = 64
+LR = 0.00
 
-# 定义数据预处理方式
 transform = transforms.ToTensor()
 
-# 定义训练数据集
 trainset = tv.datasets.MNIST(
     root='./data/',
     train=True,
     download=True,
     transform=transform)
 
-# 定义训练批处理数据
 trainloader = torch.utils.data.DataLoader(
     trainset,
     batch_size=BATCH_SIZE,
     shuffle=True,
     )
 
-# 定义测试数据集
 testset = tv.datasets.MNIST(
     root='./data/',
     train=False,
     download=True,
     transform=transform)
 
-# 定义测试批处理数据
 testloader = torch.utils.data.DataLoader(
     testset,
     batch_size=BATCH_SIZE,
@@ -53,52 +48,25 @@ testloader = torch.utils.data.DataLoader(
 
 net = Model.LeNet().to(device)
 
-# Load The PreTrain Weight
 Checkpoint = torch.load("./Model/net_008.pth")  # CPU
 para = net.parameters()
 net.load_state_dict(Checkpoint)
 
-# net = net.eval()
-# net.fuse_model()
+net.fuse_model()
 
-criterion = nn.CrossEntropyLoss()  # 交叉熵损失函数，通常用于多分类问题上
+criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(para, lr=LR, momentum=0.9)
 
-#Quantization
 net.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm')
 torch.quantization.prepare_qat(net, inplace=True)
 
-def Save_Quantize_Para(File, quantized_model):
-    File.write("Quant: ")
-    File.write(str(quantized_model.quant.scale) + "\n")
-    File.write(str(quantized_model.quant.zero_point) + "\n")
-    File.write("Conv1: ")
-    for i in range(0, 3):
-        File.write(str(quantized_model.conv1[i]) + "\n")
-    File.write("Conv2: ")
-    for i in range(0, 3):
-        File.write(str(quantized_model.conv2[i]) + "\n")
-    File.write("FC1: ")
-    for i in range(0, 2):
-        File.write(str(quantized_model.fc1[i]) + "\n")
-    File.write("FC2: ")
-    for i in range(0, 2):
-        File.write(str(quantized_model.fc2[i]) + "\n")
-    File.write("FC3: ")
-    File.write(str(quantized_model.fc3) + "\n")
-    File.write("DeQuant: ")
-    File.write(str(quantized_model.dequant) + "\n")
-
-# 训练
 if __name__ == "__main__":
     for epoch in range(EPOCH):
         sum_loss = 0.0
-        # 数据读取
         for i, data in enumerate(trainloader):
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
 
-            # 梯度清零
             optimizer.zero_grad()
 
             # forward + backward
@@ -107,7 +75,6 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
 
-            # 每训练100个batch打印一次平均loss
             sum_loss += loss.item()
             if i % 100 == 99:
                 print('[%d, %d] loss: %.03f'
@@ -122,7 +89,6 @@ if __name__ == "__main__":
         quantized_model = torch.quantization.convert(net.eval(), inplace=False)
         torch.jit.save(torch.jit.script(quantized_model), "./Model/Quant_Model_Epoch_%s.pth" % str(epoch + 1))
 
-        # 每跑完一次epoch测试一下准确率
         with torch.no_grad():
             correct = 0
             total = 0
@@ -130,14 +96,7 @@ if __name__ == "__main__":
                 images, labels = data
                 images, labels = images.to(device), labels.to(device)
                 outputs = quantized_model(images)
-                # 取得分最高的那个类
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum()
-            print('第%d个Epoch的识别准确率为: %.02f%%' % (epoch + 1, (100 * correct / total)))
-        File = open("./Model/Quant_Model_Epoch_%s.txt" % str(epoch + 1), "w")
-        Save_Quantize_Para(File, quantized_model)
-        File.close()
-        # torch.quantization.get_observer_dict(mod = net, target_dict = Quantize_Para)
-        # print (Quantize_Para)
-        # np.save("./Model/Quant_Model_Epoch_%s.npy" % str(epoch + 1), Quantize_Para)
+            print('第%d个Epoch的识别准确率为: %.02f%%' % (epoch + 1, (100 * float(correct) / float(total))))
